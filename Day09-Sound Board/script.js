3e5c9fa5ae8bb614189e908fa13c8415df68b0c2
FallenCap <fallencap18@gmail.com>
2023-02-16 19:17:30 +0530

    Day 9

---
+++ Day09-Sound Board/script.js
@@ -0,0 +1,21 @@
+const sounds = ["Valorant theme", "Multi kill", "Ace", "Teleporter", "Flawless"];
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+  });
+}
